d021ac21fb986d723256d8bab1294556ff2a17a0
lin <451617643@qq.com>
2019-03-27 23:34:45 +0800

    新增方法

--- git-test/src/com/lin/git/Test1.java
+++ git-test/src/com/lin/git/Test1.java
@@ -3,5 +3,11 @@ package com.lin.git;
 public class Test1 {
     public static void main(String[] args){
         System.out.println("hello git");
+        test();
     }
+
+    private static void test() {
+        System.out.println("test");
+    }
+
 }
